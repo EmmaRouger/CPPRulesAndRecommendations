15fab359c1b61437064365bd4ecb33dda5b0f042
EmmaRouger <58708911+EmmaRouger@users.noreply.github.com>
2024-03-13 21:17:47 -0500

    Update MEM51.cpp

---
+++ CPPrules/MEM51.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int *array = new int[3];
+    array[0]=array[1]=array[2]=2;
+    std::cout << array[0] +","+array[1]+","+array[2]+" this is the array"<< std::endl;
+    delete[] array
+
+    std::cout << "array deleted" << std::endl;
+
+    return 0;
+}
