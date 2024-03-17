374e3237bd476a87dd2f2299692abca94fced4ab
asolo726 <dcking4@ilstu.edu>
2024-03-16 21:51:43 -0500

    Updated Gamble Method. Changed line 2

--- ProgramThing/main.cpp
+++ ProgramThing/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "ATMAccount.h"
+#include "ATMAccount.cpp"
 #include <random>
 // using namespace std;
 
-void gamble (int amount);
+void gamble (ATMAccount account);
 
 int main(){
 
@@ -23,16 +23,33 @@ int main(){
 /*
 
     Insert amount you want to gamble. Guess a number between 1 & 10.
-    Rules Used: MSC51, MSC52, EXP54
+    Rules Used: MSC51, EXP54
 
 */
-void gamble(int amount){
+void gamble(ATMAccount account){
 
     std::cout << "If you guess the lucky number, you can get back triple of your money back.\n" <<
     "If you guess the unlucky number, you lose double the amount you gambled!\n" <<
     "If you get neither, you lose the amount you gambled\n" <<
-    "Always remember: there is no losing in gambling, only quitting!\n" <<
-    "Please type a number between 1 & 10: ";
+    "Always remember: there is no losing in gambling, only quitting!\n";
+    
+    int gamble = 0;
+    
+    cout << "Enter an amount to gamble: ";
+    cin >> gamble;
+    bool validAmount = false;
+    while (!validAmount)
+    {
+        if (gamble > 0 && gamble <= account.checkBalance())
+        {
+            validAmount = true;
+        }
+        else{
+            cout << "Invalid, please enter an amount to gamble: ";
+            cin >> gamble;
+        }  
+    }
+    
 
     random_device fate;
     minstd_rand0 engine(fate());
@@ -45,16 +62,37 @@ void gamble(int amount){
         int unlucky = engine() % 10;
     }
 
+    cout << "\nPlease type a number between 0 & 10: ";
+
     int input = 0;
     bool validInput = false;
+    cin >> input;
+    
     while (!validInput){
-
-        cin >> input;
-        while ()
+        if (input >= 0 && input <= 10)
         {
-            /* code */
+            validInput = true;
         }
+        else
+        {
+            cout << "\nInvalid guess. Please type a numnber between 0 & 10: ";
+            cin >> input;
+        }
+    }
 
-
+    if (input == lucky) // Big Win
+    {
+        cout << "You got it! Lucky you! Depositing: " << gamble*3 << " into your account.\n";
+        account.deposit(gamble*3);         
+    }
+    else if (input == unlucky) //Big Lose
+    {
+        cout << "Yeouch! How unfortunate, you got the unlucky number... Removing: " << gamble*2 << " from your account.\n";
+        account.withdrawal(gamble*2); 
+    }
+    else // Lose 
+    {
+        cout << "Darn. Ya didn't get the unlucky number atleast! Removing: " << gamble << " from your account.\n";
+        account.withdrawal(gamble); 
     }
 }
