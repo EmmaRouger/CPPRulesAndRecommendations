ef4f57ed23c70dbfd13c491d430cf4a3854108a1
Jessieduf <113367526+Jessieduf@users.noreply.github.com>
2024-03-17 22:15:59 -0500

    Add files via upload

---
+++ CPPrecomendations/EXP15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+  srand(time(0));
+  int num = rand()%100;
+  if(num<50){ // Don't put a semicolon after an if statement
+    cout<<"The number is larger than 50"<<endl;
+  }else{
+    cout<<"The number is smaller than 50"<<endl;
+  }
+
+  return 0;
+}
---
+++ CPPrecomendations/PRE01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+#define AREA(a, b) (a*b)
+int main(){
+  int length = 5;
+  int width = 2;
+  int area = AREA(length, width);
+  cout<<"Area of a rectangle with length = "<<length<<" and width = "<<width<<" : "<<area<<endl;
+
+  return 0;
+}
