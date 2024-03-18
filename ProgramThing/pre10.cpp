01f98527f20b30a735e9cb3ab4ebc4d19c57a7f3
katiejohnson789 <162757490+katiejohnson789@users.noreply.github.com>
2024-03-17 19:21:45 -0500

    add code for katie and emma unusual recs - macros

---
+++ ProgramThing/pre10.cpp
@@ -0,0 +1,24 @@
+//Recommendation PRE10-C
+
+#include <iostream>
+
+#define SWAP(x, y) \
+  do { \
+    if ((x) < (y)) { \
+      int temp = (x); \
+      (x) = (y); \
+      (y) = temp; \
+    } else { \
+      std::cout<<"The first number is greater than the second"<<std::endl; \
+      std::exit(1); \
+    } \
+  } while(0)
+
+int main() {
+  int m = 20;
+  int n = 10;
+  std::cout<<"Prior to swap, first number = "<<m<<" and second number = "<<n<<std::endl;
+  SWAP(m, n);
+  std::cout<<"After swap, first number = "<<m<<" and second number = "<<n<<std::endl;
+  return 0;
+}
