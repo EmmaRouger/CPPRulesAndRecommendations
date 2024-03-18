fb808439d746cec4d2e7a46fc5233069ce24ee17
EmmaRouger <58708911+EmmaRouger@users.noreply.github.com>
2024-03-17 23:26:18 -0500

    Update MEM57.cpp

--- CPPrules/MEM57.cpp
+++ CPPrules/MEM57.cpp
@@ -1,8 +1,35 @@
 
 #include <iostream>
-#include <vector>
+#include <cstdlib>
+
+
+struct alignas(16) my_struct{
+    int a;
+    int b;
+
+    static void *operator new(size_t nbytes)
+    {
+        if(void *ptr=_aligned_malloc(alignof(my_struct),nbytes))
+        {
+            return ptr;
+        }
+        throw std::bad_alloc();
+        
+    }
+    static void operator delete(void *ptr) 
+    {
+        std::cout<<"deallocating ptr"<<std::endl;
+        free(ptr);
+    }
+};
+
+
 
 int main()
 {
-    
+    my_struct* myStruct = new my_struct;
+
+    std::cout << "Address of Structure: " << myStruct << std::endl;
+
+    delete(myStruct);
 }
